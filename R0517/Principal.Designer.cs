9eeafd83b864a9b28582eee4399627af5b947840
John Ortiz <johnortizo@outlook.com>
2015-07-21 11:29:00 -0500

    Receta C# No. 5.17: Mostrar Diálogos de Archivos Comunes

---
+++ R0517/Principal.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace R0517
+{
+    partial class frmR0517
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbContenido = new System.Windows.Forms.RichTextBox();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtbContenido
+            // 
+            this.rtbContenido.Location = new System.Drawing.Point(13, 13);
+            this.rtbContenido.Name = "rtbContenido";
+            this.rtbContenido.Size = new System.Drawing.Size(443, 134);
+            this.rtbContenido.TabIndex = 0;
+            this.rtbContenido.Text = "";
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.Location = new System.Drawing.Point(463, 13);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(94, 29);
+            this.btnAbrir.TabIndex = 1;
+            this.btnAbrir.Text = "Abrir";
+            this.btnAbrir.UseVisualStyleBackColor = true;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(463, 49);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(94, 29);
+            this.btnGuardar.TabIndex = 2;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(463, 85);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(94, 29);
+            this.btnSalir.TabIndex = 3;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // frmR0517
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 159);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnAbrir);
+            this.Controls.Add(this.rtbContenido);
+            this.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "frmR0517";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "R0517";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbContenido;
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ R0517/Principal.cs
@@ -0,0 +1,67 @@
+﻿// OrtizOL - xCSw - http://ortizol.blogspot.com
+
+using System;
+using System.Windows.Forms;
+
+namespace R0517
+{
+    public partial class frmR0517 : Form
+    {
+        public frmR0517()
+        {
+            InitializeComponent();
+        }
+
+        #region Eventos
+        /// <summary>
+        /// Muestra diálogo de apertura de archivo.
+        /// </summary>
+        /// <param name="sender">Objeto generador del evento.</param>
+        /// <param name="e">Argumentos del evento.</param>
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofdAbrir = new OpenFileDialog();
+            // Filtros de selección de archivo:
+            ofdAbrir.Filter = "Archivos de texto enriquecido (*.rtf)|*RTF|Todos los archivos (*.*)|*.*";
+            // Comprobación de existencia de archivo:
+            ofdAbrir.CheckFileExists = true;
+            // Ruta inicial:
+            ofdAbrir.InitialDirectory = @"C:\";
+
+            // Valida que el usuario haya presionado el botón Aceptar (OK):
+            if (ofdAbrir.ShowDialog() == DialogResult.OK)
+            {
+                rtbContenido.LoadFile(ofdAbrir.FileName);
+            }
+        }
+        /// <summary>
+        /// Muestra diálogo de guardado de archivo.
+        /// </summary>
+        /// <param name="sender">Objeto generador del evento.</param>
+        /// <param name="e">Argumentos del evento.</param>
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfdGuardar = new SaveFileDialog();
+            // Filtros de guardado de archivo:
+            sfdGuardar.Filter = "Archivos de texto enriquecido (*.rtf)|*RTF|Archivos de texto (*.txt)|*TXT|Todos los archivos (*.*)|*.*";
+            // Ruta inicial:
+            sfdGuardar.InitialDirectory = @"C:\";
+
+            // Valida que el usuario haya presionado el botón Aceptar (OK):
+            if (sfdGuardar.ShowDialog() == DialogResult.OK)
+            {
+                rtbContenido.SaveFile(sfdGuardar.FileName);
+            }
+        }
+        /// <summary>
+        /// Evento de cierre la aplicación.
+        /// </summary>
+        /// <param name="sender">Objeto generador del evento.</param>
+        /// <param name="e">Argumentos del evento.</param>
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        #endregion
+    }
+}
